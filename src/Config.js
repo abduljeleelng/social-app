c18fa32e351d1669a0a3883401ed141a7ac87c0e
abduljeleelng <abduljeleelng@gmail.com>
2020-05-14 21:51:48 +0100

    chnage from local end point to server endpoint

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
-//export const API = "https://catholicapi.herokuapp.com/api";
+export const API = "https://catholicapi.herokuapp.com/api";
 
-export const API = "http://localhost:8080/api";
+///export const API = "http://localhost:8080/api";
