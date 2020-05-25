861316d26f98e25b7101688c7917f66dda00333e
abduljeleelng <abduljeleelng@gmail.com>
2020-05-26 00:30:50 +0100

    Footer

---
+++ src/screen/newsfeed/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="bg-white iq-footer">
+            <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-6">
+                <ul className="list-inline mb-0">
+                    <li className="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
+                    <li className="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                </ul>
+                </div>
+                <div className="col-lg-6 text-right">
+                Copyright 2020 <a href="#">SocialV</a> All Rights Reserved.
+                </div>
+            </div>
+            </div>
+        </footer>
+    )
+}
--- src/screen/timeline/TimeLine.js
+++ src/screen/timeline/TimeLine.js
@@ -3849,30 +3849,13 @@ export default class TimeLine extends Component {
               </div>
             </div>
           </div>
-          <div className="col-sm-12 text-center">
-            <img src="images/page-img/page-load-loader.gif" alt="loader" style={{height: 100}} />
-          </div>
         </div>
       </div>
     </div>
   </div>
   {/* Wrapper END */}
   {/* Footer */}
-  <footer className="bg-white iq-footer">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-6">
-          <ul className="list-inline mb-0">
-            <li className="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
-            <li className="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
-          </ul>
-        </div>
-        <div className="col-lg-6 text-right">
-          Copyright 2020 <a href="#">SocialV</a> All Rights Reserved.
-        </div>
-      </div>
-    </div>
-  </footer>
+
 </div>
 
             </Fragment>
