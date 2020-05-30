7f730b45ea8c894ed59fa010e9e17dacad6f7013
abduljeleelng <abduljeleelng@gmail.com>
2020-05-30 18:46:54 +0100

    unuse

--- src/screen/newsfeed/file.js
+++ src/screen/newsfeed/file.js
@@ -9,30 +9,13 @@
 
 
 
-likePost=(userId, token, id)=>{
+const likePost=(userId, token, id)=>{
     const { post }= this.state;
     like(userId, token, post.id).then(data=>{if (data.error) {console.log(data.error);} else 
     {this.setState({like: !this.state.like,likes: post.likes.length});}})
 }
-unLikePost=(userId, token, id)=>{
+const unLikePost=(userId, token, id)=>{
     const { post }= this.state;
     unlike(userId, token, post.id).then(data=>{if (data.error) {console.log(data.error);} else 
     {this.setState({like: !this.state.like,likes: post.likes.length});}})
-}
-
-
-
-
-
-render(){
-//const posts = this.props.posts
-//const {like,likes,posts}=this.state;
-///console.log(JSON.stringify(posts));
-return (
-  <Fragment>
-    {
-      this.renderPost()
-    }
-  </Fragment>
-)
 }
