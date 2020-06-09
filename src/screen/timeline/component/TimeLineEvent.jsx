5bff46581286ef4d310bfb3b8f8610ec71f756b6
abduljeleelng <abduljeleelng@gmail.com>
2020-06-09 13:13:05 +0100

    photo

--- src/screen/timeline/component/TimeLineEvent.jsx
+++ src/screen/timeline/component/TimeLineEvent.jsx
@@ -47,14 +47,6 @@ export default function TimeLineEvent() {
                         <div className="iq-card-body">
                           <ul className="profile-img-gallary d-flex flex-wrap p-0 m-0">
                             <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g1.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g2.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g3.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g4.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g5.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-3"><a href="javascript:void();"><img src="images/page-img/g6.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-0"><a href="javascript:void();"><img src="images/page-img/g7.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-0"><a href="javascript:void();"><img src="images/page-img/g8.jpg" alt="gallary-image" className="img-fluid" /></a></li>
-                            <li className="col-md-4 col-6 pl-2 pr-0 pb-0"><a href="javascript:void();"><img src="images/page-img/g9.jpg" alt="gallary-image" className="img-fluid" /></a></li>
                           </ul>
                         </div>
                       </div>
