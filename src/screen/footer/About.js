e0357669f26caee8e5aa6d2ac96ffc033354a15d
abduljeleelng <abduljeleelng@gmail.com>
2020-06-19 21:50:39 +0100

    update contents

--- src/screen/footer/About.js
+++ src/screen/footer/About.js
@@ -27,12 +27,12 @@ export default class About extends Component {
         return (
             <div>
                 <ul class="cb-slideshow">
-                    <li><span>Image 01</span><div><h3>re·lax·a·tion</h3></div></li>
-                    <li><span>Image 02</span><div><h3>qui·e·tude</h3></div></li>
-                    <li><span>Image 03</span><div><h3>bal·ance</h3></div></li>
-                    <li><span>Image 04</span><div><h3>e·qua·nim·i·ty</h3></div></li>
-                    <li><span>Image 05</span><div><h3>com·po·sure</h3></div></li>
-                    <li><span>Image 06</span><div><h3>se·ren·i·ty</h3></div></li>
+                    <li><span>Image 01</span><div><h3> Faith in God</h3></div></li>
+                    <li><span>Image 02</span><div><h3>Social with Christ</h3></div></li>
+                    <li><span>Image 03</span><div><h3>We Love christ</h3></div></li>
+                    <li><span>Image 04</span><div><h3>Uniformity</h3></div></li>
+                    <li><span>Image 05</span><div><h3>Love</h3></div></li>
+                    <li><span>Image 06</span><div><h3>Sincerity</h3></div></li>
                 </ul>
                 <SignInHeader />
                 <Container >
