e93942cf216860e69daffb98ad55bd67cfde7102
abduljeleelng <abduljeleelng@gmail.com>
2020-06-20 08:45:29 +0100

    compomne b

--- src/screen/user/Forget.js
+++ src/screen/user/Forget.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
-import BackgroundSlider from 'react-background-slider'
 import {SignInHeader,ForgetPassword,Footer} from '../../components';
-
-import bg1 from '../../asset/img/slide/bg1.jpg';
-import bg2 from '../../asset/img/slide/bg2.jpg';
-import bg3 from '../../asset/img/slide/bg3.png';
-import bg4 from '../../asset/img/slide/bg4.png';
-import bg5 from '../../asset/img/slide/bg5.jpg';
-
 
 export default class Forget extends Component {
     render() {
@@ -24,11 +16,6 @@ export default class Forget extends Component {
                 <SignInHeader />
                 <ForgetPassword/>
                 <Footer />
-                <BackgroundSlider
-                    images={[bg1,bg2,bg3,bg4,bg5]}
-                    duration={10} transition={2} 
-                />
-                
             </div>
         )
     }
