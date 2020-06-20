93875880105a49321b96e866d9d51a537e01a268
abduljeleelng <abduljeleelng@gmail.com>
2020-06-20 08:43:16 +0100

    comp

--- src/screen/footer/Prayer.js
+++ src/screen/footer/Prayer.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import BackgroundSlider from 'react-background-slider'
+
 import {SignInHeader, Footer} from '../../components';
 
-
-import bg1 from '../../asset/img/slide/bg1.jpg';
-import bg2 from '../../asset/img/slide/bg2.jpg';
-import bg3 from '../../asset/img/slide/bg3.png';
-import bg4 from '../../asset/img/slide/bg4.png';
-import bg5 from '../../asset/img/slide/bg5.jpg';
 import { Col,Container,Row } from 'reactstrap';
 import prayer from '../../asset/img/beech.jpg';
 
@@ -37,10 +31,6 @@ export default class Prayer extends Component {
                 </Row>
                 </Container>
                 <Footer />
-                <BackgroundSlider
-                    images={[bg1,bg2,bg3,bg4,bg5]}
-                    duration={10} transition={2} 
-                />
             </div>
         )
     }
