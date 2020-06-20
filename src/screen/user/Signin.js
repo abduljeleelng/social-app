a3ed1913156b411d630fef667133358f16f97d85
abduljeleelng <abduljeleelng@gmail.com>
2020-06-20 08:40:57 +0100

    remove compemts

--- src/screen/user/Signin.js
+++ src/screen/user/Signin.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import BackgroundSlider from 'react-background-slider';
-//import BackgroundSlideshow from 'react-background-slideshow'
+
 import {SignInHeader, Create,Footer} from '../../components';
 
-import bg1 from '../../asset/img/slide/bg1.jpg';
-import bg2 from '../../asset/img/slide/bg2.jpg';
-import bg3 from '../../asset/img/slide/bg3.png';
-import bg4 from '../../asset/img/slide/bg4.png';
-import bg5 from '../../asset/img/slide/bg5.jpg';
+
 
 
 export default class Signin extends Component {
     render() {
         return (
             <div>
-                <BackgroundSlider
-                    images={[bg1,bg2,bg3,bg4,bg5]}
-                    duration={10} transition={2} 
-                />
                 <ul class="cb-slideshow">
                     <li><span>Image 01</span><div><h3> Faith in God</h3></div></li>
                     <li><span>Image 02</span><div><h3>Social with Christ</h3></div></li>
