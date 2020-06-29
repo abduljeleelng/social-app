d811c57abfa35df77cf3d3e50034f80f95c4c1ee
abduljeleelng <abduljeleelng@gmail.com>
2020-06-29 13:00:47 +0100

    styling images

--- src/screen/footer/Help.js
+++ src/screen/footer/Help.js
@@ -29,7 +29,7 @@ export default class Help extends Component {
                     <Col xs="12" md="3" sm="12" lg="3" xl="3" ></Col>
                     <Col xs="12" md="3" sm="12" lg="6" xl="6"  className="center" style={{margin:20,marginBottom:100,  backgroundColor:'#000',padding:20,borderRadius:20,opacity:0.9,boxShadow:1}}>
                         <h1 style={{alignContent:"center",margin:25}}>Why <b>Iamacatholic</b> Exists</h1>
-                        <img src={catho} alt="catho" className="img-fluid" style={{alignSelf:'center', margin:15}} />
+                        <img src={catho} alt="catho" width="200" height="200" className="img-fluid" style={{alignSelf:'center', margin:15}} />
                     <p style={{fontSize:20}}>
                     Iamacatholic’s mission is to bring together all Catholics worldwide under
                     one unique community and grow. We want to connect the people who have the 
